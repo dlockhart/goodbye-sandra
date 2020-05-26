38eb2207d87bbeb6b35b744a80359c6bd6dc2e58
Dave Lockhart <dave.lockhart@d2l.com>
2020-05-26 18:29:17 -0400

    basic dialog view

--- src/my-courses-widget.js
+++ src/my-courses-widget.js
@@ -27,13 +27,6 @@ export class MyCoursesWidget extends LitElement {
 	}
 
 	render() {
-		const activeCourseData = courses.find((c) => c.id === this._activeCourseId);
-		let dialogTitle = '';
-		let dialogOpened = false;
-		if (activeCourseData) {
-			dialogTitle = activeCourseData.name;
-			dialogOpened = true;
-		}
 		return html`
 			<goodbye-sandra-widget name="My Courses" @goodbye-sandra-course-card-select="${this._handleCourseCardSelect}">
 				<div class="card-grid">
@@ -45,13 +38,39 @@ export class MyCoursesWidget extends LitElement {
 					}))}
 				</div>
 			</goodbye-sandra-widget>
-			<d2l-dialog title-text="${dialogTitle}" ?opened="${dialogOpened}" @d2l-dialog-close="${this._handleDialogClose}">
-			</d2l-dialog>
+			${this._getDialogView()}
 		`;
+	}
+
+	_getDialogView() {
+		
+		const activeCourseData = courses.find((c) => c.id === this._activeCourseId);
+
+		let title = '';
+		let opened = false;
+		let content = null;
+		if (activeCourseData) {
+			title = activeCourseData.name;
+			opened = true;
+			content = html`
+				<img src="assets/course-images/${activeCourseData.image}-lg.png" style="width: 100%;">
+				<p>Hello</p>
+			`;
+		}
+		
+		return html`
+			<d2l-dialog title-text="${title}" ?opened="${opened}" @d2l-dialog-close="${this._handleDialogClose}">
+				${content}
+			</d2l-dialog>`;
+
 	}
 
 	_handleCourseCardSelect(e) {
 		this._activeCourseId = e.detail.id;
+		// TODO: fix this somehow
+		setTimeout(() => {
+			this.shadowRoot.querySelector('d2l-dialog').resize();
+		}, 100);
 	}
 
 	_handleDialogClose() {
